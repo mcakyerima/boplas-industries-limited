91f88f3f17af6cdc3f1913855e3b8034f569464e
mcakeyerima <mcakyerima@gmail.com>
2022-01-30 22:32:42 +0100

    commit

--- pages/index.js
+++ pages/index.js
@@ -16,8 +16,6 @@ export default function Home() {
       </Head>
       <Featured/>
       <Products/>
-
-
     </div>
     )
     }
--- pages/product/[id].jsx
+++ pages/product/[id].jsx
@@ -1,16 +1,19 @@
-import styles from "../styles/Featured.module.css";
+import styles from "/styles/Product.module.css";
 import Image from "next/image"
+import { useState } from "react";
 
 const Product = () => {
     const product = {
         id : 1,
-        name : 'bowl',
+        name : 'BOWL',
         quantity : 100,
-        desc : "the quick brown fox jumps over the lazy dogs",
+        desc : "the quick brown fox jumps over the lazy dogs, this is just  an example text describing the product before the actual product description fetched from the api",
         rating : 2,
-        price : [1.2 , 2.5 , 3.5],
+        price : [1500 , 6000 ,6500],
         img : "/img/bowl.png"
     };
+
+    const [price , setPrice] = useState(0);
 
     return <div className={styles.container}>
         <div className={styles.left}>
@@ -19,7 +22,54 @@ const Product = () => {
             </div>
         </div>
         <div className={styles.right}>
-            <h1>this is {product.name}</h1>
+            <h1>{product.name}</h1>
+            <span className={styles.price}>N{product.price[price]}</span>
+            <p className={styles.desc}>{product.desc}</p>
+            <h3 className={styles.choose}>Colors</h3>
+            {/* <div className={styles.colors}>
+                <div className={styles.color}></div>
+                <div className={styles.color}></div>
+                <div className={styles.color}></div>
+            </div> */}
+            <div className={styles.colors}>
+                        <div className={styles.c_blue}><span></span></div>
+                        <div className={styles.c_red}><span></span></div>
+                        <div className={styles.c_green}><span></span></div>
+            </div>
+            
+            <p className={styles.header}>Choose Colors</p>
+            <div className={styles.colorOptions}>
+                <div className={styles.options}>
+                    <input
+                    type="checkbox"
+                    id="blue"
+                    name="blue"
+                    className={styles.checkbox}
+                    />
+                    <label htmlFor="blue">Blue</label>
+                </div>
+                <div className={styles.options}>
+                    <input
+                    type="checkbox"
+                    id="green"
+                    name="green"
+                    className={styles.checkbox}
+                    />
+                    <label htmlFor="green">Green</label>
+                </div> <div className={styles.options}>
+                    <input
+                    type="checkbox"
+                    id="orange"
+                    name="orange"
+                    className={styles.checkbox}
+                    />
+                    <label htmlFor="orange">Orange</label>
+                </div>
+            </div>
+            <div className={styles.add}>
+                <input type="number" defaultValue={1} className={styles.quantity}/>
+                <button className={styles.button}>Add to cart</button>
+            </div>
         </div>
 
     </div>
